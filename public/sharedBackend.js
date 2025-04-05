7eaaade160b7601d0b5a9fc508159ade1c04e5b9
meghanadhgoud <meghanadhgoud9@gmail.com>
2025-04-06 04:36:44 +0530

    added render url

--- public/sharedBackend.js
+++ public/sharedBackend.js
@@ -1,5 +1,7 @@
 // sharedBackend.js
-const socket = io('http://localhost:3000');
+// sharedBackend.js
+// const socket = io('http://localhost:3000');
+const socket = io('https://dddd-bx1p.onrender.com');
 
 const sharedBackend = {
     buses: [],
